48ccbea08f94a863d8f04caadc939b1d39bc894b
Shafiq Marediya <shafiq702@gmail.com>
2015-12-05 19:43:48 -0500

    first user complete

--- models/index.js
+++ models/index.js
@@ -1,28 +1,28 @@
-var mongoose = require('mongoose'); 
-console.log(mongoose); 
-mongoose.connect('mongodb://localhost/budgeteer'); 
+var mongoose = require('mongoose');
+console.log(mongoose);
+mongoose.connect('mongodb://localhost/budgeteer');
 
-var Schema = mongoose.Schema; 
+var Schema = mongoose.Schema;
 
-mongoose.connection.on('error', console.error.bind(console)); 
+mongoose.connection.on('error', console.error.bind(console));
 
 
 var transactionSchema = new Schema ({
-	amount: { type: Number, required: true }, 
-	baseType: { type: String }, 
+	amount: { type: Number, required: true },
+	baseType: { type: String },
 	category: { type: String, required: true }, // dining or entertainment...
-	date: { type: String, required: true }, // '2014-10-14', 
+	date: { type: String, required: true }, // '2014-10-14',
 	container: { type: String, required: true }
-}); 
+});
 
 transactionSchema.statics.findCreditTimeSeries = function(baseType, container) {
-  //console.log(baseType); 
+  //console.log(baseType);
   return this.find({ baseType: baseType, container : container }).exec()
 		.then(function(timeSeries) {
       return timeSeries.map(function(transaction) {
         return {
-          date: transaction.date, 
-          amount: transaction.amount, 
+          date: transaction.date,
+          amount: transaction.amount,
           container: transaction.container
         }
       })
@@ -30,15 +30,15 @@ transactionSchema.statics.findCreditTimeSeries = function(baseType, container) {
 }
 
 transactionSchema.statics.findCategoryTimeSeries = function(baseType) {
-  console.log('in statics'); 
+  console.log('in statics');
   return this.find({ baseType: baseType }).exec()
     .then(function(timeSeries) {
       console.log('in statics: ', timeSeries)
       return timeSeries.map(function(transaction) {
-        return { 
-          date: transaction.date, 
-          amount: transaction.amount, 
-          container: transaction.container, 
+        return {
+          date: transaction.date,
+          amount: transaction.amount,
+          container: transaction.container,
           category: transaction.category
         }
       })
@@ -46,6 +46,6 @@ transactionSchema.statics.findCategoryTimeSeries = function(baseType) {
 }
 
 
-var Transaction = mongoose.model('transaction', transactionSchema); 
+var Transaction = mongoose.model('transaction', transactionSchema);
 
-module.exports = Transaction; 
+module.exports = Transaction;
--- seed.js
+++ seed.js
@@ -1,18 +1,18 @@
 
 
 // This file should contain all the record creation needed to seed the database with its default values.
-// The data can then be loaded with the node seed.js 
+// The data can then be loaded with the node seed.js
 
 var Promise = require('bluebird');
 var mongoose = require('mongoose');
-var models = require('./models/transaction.js');
-var Transaction = models.Transaction;
+var Transaction = require('./models/index.js');
+//var Transaction = models.Transaction;
 
 
-mongoose.connect('mongodb://localhost/budgeteer'); 
+mongoose.connect('mongodb://localhost/budgeteer');
 
 
-var userData = require('./userData.js'); 
+var userData = require('./userData.js');
 
 mongoose.connection.on('open', function() {
   mongoose.connection.db.dropDatabase(function() {
@@ -22,20 +22,20 @@ mongoose.connection.on('open', function() {
     var mapOfCreatedDocuments = userData.transaction.map(function(transaction) {
 
       return Transaction.create({
-        amount: transaction.amount.amount, 
-        baseType: transaction.baseType, 
-        category: transaction.category, 
-        date: transaction.date.date, 
-        container: transaction.CONTAINER   
+        amount: transaction.amount.amount,
+        baseType: transaction.baseType,
+        category: transaction.category,
+        date: transaction.date.date,
+        container: transaction.CONTAINER
       })
     })
 
     Promise.all(mapOfCreatedDocuments)
       .then(function() {
-        mongoose.connection.close(); 
+        mongoose.connection.close();
     })
 
-    console.log('Added data'); 
+    console.log('Added data');
     // Promise.map(Object.keys(data), function(modelName) {
     //   return Promise.map(data[modelName], function(item) {
     //     return models[modelName].create(item);
