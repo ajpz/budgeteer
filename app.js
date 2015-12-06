4425950f12145e8a130ec9fd6231c80a916c505e
Victor Hom <vh62@cornell.edu>
2015-12-06 09:34:30 -0500

    trying to get swig up

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
-var bodyParser = require('body-parser'); 
-var express = require('express'); 
-var morgan = require('morgan'); 
-var bluebird = require('bluebird'); 
-var path = require('path'); 
+var bodyParser = require('body-parser');
+var express = require('express');
+var morgan = require('morgan');
+var bluebird = require('bluebird');
+var path = require('path');
+var swig = require('swig');
 
-
-var app = express(); 
+var app = express();
 
 // set-up morgan, body-parser, express.static
-app.use(morgan('dev')); 
-app.use(express.static(path.join(__dirname + '/public'))); 
-app.use(bodyParser.json()); 
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false }));
 
-app.use('/', require('./routes')); 
+//SWIG Stuff
+// point res.render to the proper directory
+app.set('views', __dirname + '/public');
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files
+// have it use swig to do so
+app.engine('html', swig.renderFile);
+// turn of swig's caching
+swig.setDefaults({cache: false});
+
+
+app.use('/', require('./routes'));
 
 app.use('/api', require('./routes/api'));
 
 app.use(function(req, res, next) {
-	var err = new Error('Not Found'); 
-	err.status = 404; 
-	next(err); 
-}) 
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+})
 
 app.use(function(err, req, res, next) {
-	err.status = err.status || 500; 
-	res.status(err.status).send(); 
+	err.status = err.status || 500;
+	res.status(err.status).send();
 })
 
 app.listen(3000, function(){
-	console.log('Listening on port 3000'); 
+	console.log('Listening on port 3000');
 })
-
-
