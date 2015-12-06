d1e8c78415d69f57d3f0e36da64287d2fc37d233
ajpz <perez.jonthan@gmail.com>
2015-12-05 19:29:58 -0500

    Add working routes

--- models/index.js
+++ models/index.js
@@ -15,7 +15,35 @@ var transactionSchema = new Schema ({
 	container: { type: String, required: true }
 }); 
 
-// transactionSchema = 
+transactionSchema.statics.findCreditTimeSeries = function(baseType, container) {
+  //console.log(baseType); 
+  return this.find({ baseType: baseType, container : container }).exec()
+		.then(function(timeSeries) {
+      return timeSeries.map(function(transaction) {
+        return {
+          date: transaction.date, 
+          amount: transaction.amount, 
+          container: transaction.container
+        }
+      })
+    })
+}
+
+transactionSchema.statics.findCategoryTimeSeries = function(baseType) {
+  console.log('in statics'); 
+  return this.find({ baseType: baseType }).exec()
+    .then(function(timeSeries) {
+      console.log('in statics: ', timeSeries)
+      return timeSeries.map(function(transaction) {
+        return { 
+          date: transaction.date, 
+          amount: transaction.amount, 
+          container: transaction.container, 
+          category: transaction.category
+        }
+      })
+    })
+}
 
 
 var Transaction = mongoose.model('transaction', transactionSchema); 
--- routes/api.js
+++ routes/api.js
@@ -9,14 +9,33 @@ module.exports = api;
 // /api/
 api.get('/users/:id/transactions/', function(req, res, next) {
   var _id = req.params.id; 
-  // console.log(Transaction); 
+  
 	Transaction.find({}).exec() //add transaction
 		.then(function(transactions) {
-      console.log('arrive here'); 
-      // console.log(transactions)
       res.json(transactions); 
     })
     .then(null, next); 
 })
 
+api.get('/users/:id/transactions/:container/:baseType', function(req, res, next) {
+	Transaction.findCreditTimeSeries(req.params.baseType.toUpperCase(), req.params.container)
+		.then(function(ts) {
+			res.json(ts); 
+		})
+})
+
+api.get('/users/:id/transactions/:baseType/category/all', function(req, res, next) {
+	console.log('hitting route'); 
+	Transaction.findCategoryTimeSeries(req.params.baseType.toUpperCase())
+		.then(function(ts) {
+		console.log('here', ts)			
+			var dataObj = {};  // { Rent: [{ date: date, amoutn: amount }] } 
+			ts.forEach(function(tx) {
+				console.log('hahah', tx); 
+				if(!dataObj[tx.category]) dataObj[tx.category] = []; 
+				dataObj[tx.category].push(tx); 
+			})
+			res.json(dataObj); 
+		})	
+}) 
 
